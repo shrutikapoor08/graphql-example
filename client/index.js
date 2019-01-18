d6fe439f67b7f05f1bb4740ef1384cc5365ebb0f
Shruti Kapoor <shrkapoor@paypal.com>
2019-01-17 16:35:17 -0800

    Adding client

---
+++ client/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ApolloClient from 'apollo-client';
+import { ApolloProvider } from 'react-apollo';
+
+const client = new ApolloClient({});
+
+const Root = () => {
+    return (
+        <ApolloProvider client = {client}>
+            <div> Test </div>
+        </ApolloProvider>
+    );
+};
+
+ReactDOM.render(
+    <Root />,
+    document.querySelector('#root')
+);
