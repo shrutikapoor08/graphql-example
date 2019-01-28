41f61c8b76f274e13a0963466d4167156f7c7378
Shruti Kapoor <shrkapoor@paypal.com>
2019-01-28 13:52:56 -0800

    Adding eslint

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+    entry: {
+        app: './client/index.js'
+    },
+    output: {
+        path: path.resolve(__dirname,'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                loader: 'babel-loader',
+                exclude: /(node_modules)/
+            },
+            {
+                test: /\.(js|jsx)$/,
+                loader: 'eslint-loader',
+                exclude: /(node_modules)/
+            },
+
+        ]
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    devServer: {
+        contentBase: './dist'
+    }
+};
